91e115537042f43fd93ec7225e9732cded458489
Alexey Rumyantsev <dev@bpdu.ru>
2024-11-02 15:43:59 +0300

    Revert "Realize the methods of ArrayStorage"
    This reverts commit 161f7d8bc34c497ffd7475ed44fbaf5167b127bc.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,60 +3,28 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int index = 0;
 
     void clear() {
-        for (int i = 0; i < index; i++) {
-            storage[i] = null;
-            index = 0;
-        }
     }
 
     void save(Resume r) {
-        if (index < 10000) {
-            storage[index] = r;
-            index++;
-        } else {
-            System.out.println("Storage is full");
-        }
     }
 
     Resume get(String uuid) {
-        Resume res = null;
-        int i = 0;
-        while (i < index && res == null) {
-            res = storage[i].toString().equals(uuid) ? storage[i] : res;
-            i++;
-        }
-        return res;
+        return null;
     }
 
     void delete(String uuid) {
-        //Delete an element
-        for (int i = 0; i < index; i++) {
-            storage[i] = storage[i].toString().equals(uuid) ? null : storage[i];
-        }
-        //Rearrange array removing null elements
-        int non_null_index=index;
-        for (int i = 0; i < index - 1; i++) {
-            if (storage[i] == null) {
-                storage[i] = storage[i + 1];
-                non_null_index--;
-            }
-        }
-        index=non_null_index;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] arr = new Resume[index];
-        System.arraycopy(storage, 0, arr, 0, index);
-        return arr;
+        return new Resume[0];
     }
 
     int size() {
-        return index;
+        return 0;
     }
 }
