034877f393567d0ee5ce5671d5c3b640bc6a9ddb
Alexey Rumyantsev <dev@bpdu.ru>
2024-11-02 15:48:49 +0300

    Realize methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,58 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int index = 0;
 
     void clear() {
+        for (int i = 0; i < index; i++) {
+            storage[i] = null;
+            index = 0;
+        }
     }
 
     void save(Resume r) {
+        if (index < 10000) {
+            storage[index] = r;
+            index++;
+        } else {
+            System.out.println("Storage is full");
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume res = null;
+        int i = 0;
+        while (i < index && res == null) {
+            res = storage[i].toString().equals(uuid) ? storage[i] : null;
+            i++;
+        }
+        return res;
     }
 
     void delete(String uuid) {
+        int notNullIndex = index;
+        int i = 0;
+        do {
+            Resume res = storage[i];
+            if (res.toString().equals(uuid) || res == null) {
+                storage[i] = storage[i + 1];
+                notNullIndex--;
+            }
+            i++;
+        } while (i < index);
+        index = notNullIndex;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] arr = new Resume[index];
+        System.arraycopy(storage, 0, arr, 0, index);
+        return arr;
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
